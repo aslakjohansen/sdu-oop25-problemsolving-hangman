590aa3a287185fa47bc6cfa6605853642c1b8f93
Aslak Johansen <aslakj@gmail.com>
2025-09-24 11:27:19 +0200

    Basic input loop

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 
 Console.WriteLine(secret);
 
+while (true) {
+  Console.WriteLine("Give us a guess: ");
+  string? input = Console.ReadLine();
+  if (input==null || input.Length!=1) {
+    Console.WriteLine("Error: Input should be 1 long");
+    continue;
+  }
+  char c = input[0];
+  Console.WriteLine(c);
+}
+
